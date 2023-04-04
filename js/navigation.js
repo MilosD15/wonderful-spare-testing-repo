996c512febc76cac897899ef96fef3935f85dfe0
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2023-04-04 15:16:00 +0200

    NAV-MENU-preview-ready-v3

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,5 @@
+import { getCSSPropertyValueFromRoot } from "./additional-func.js";
+
 if (document.querySelector("[data-navigation-menu]")) {
   // DOM elements
   const navigationMenu = document.querySelector("[data-navigation-menu]");
@@ -5,6 +7,9 @@ if (document.querySelector("[data-navigation-menu]")) {
   const navigationBgClouds = navigationMenu.querySelectorAll("[data-navigation-cloud]");
   const openMenuButton = document.querySelector(".open-menu-btn");
   const closeMenuButton = navigationMenu.querySelector(".close-menu-btn");
+
+  const fadeInDuration = parseInt(getCSSPropertyValueFromRoot("--NAVBAR-fade-in-transition-duration"));
+  const slideInBiggestDuration = parseInt(getCSSPropertyValueFromRoot("--NAVBAR-slide-in-biggest-transition-duration"));
 
   openMenuButton.addEventListener("click", () => {
     openMenuPanel();
@@ -16,23 +21,31 @@ if (document.querySelector("[data-navigation-menu]")) {
 
   function openMenuPanel() {
     navigationMenu.classList.add("show");
+
     setTimeout(() => {
       navigationMenu.classList.add("fade-in");
-      navigationNavbar.classList.add("fade-in");
       navigationBgClouds.forEach(cloud => {
         cloud.classList.add("slide-in");
       });
     }, 0);
+
+    setTimeout(() => {
+      navigationNavbar.classList.add("fade-in");
+    }, slideInBiggestDuration - fadeInDuration / 2);
   }
 
   function closeMenuPanel() {
-    navigationMenu.classList.remove("fade-in");
     navigationNavbar.classList.remove("fade-in");
     navigationBgClouds.forEach(cloud => {
       cloud.classList.remove("slide-in");
     });
+
+    setTimeout(() => {
+      navigationMenu.classList.remove("fade-in");
+    }, slideInBiggestDuration / 2);
+
     setTimeout(() => {
       navigationMenu.classList.remove("show");
-    }, 400);
+    }, slideInBiggestDuration);
   }
 }
