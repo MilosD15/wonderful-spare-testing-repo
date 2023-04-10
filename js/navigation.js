dda3fada502897517260ff252ea0f6836b79d340
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2023-04-10 13:05:26 +0200

    NAV-MENU-randomly-positioned-clouds

--- js/navigation.js
+++ js/navigation.js
@@ -4,10 +4,94 @@ if (document.querySelector("[data-navigation-menu]")) {
   // DOM elements
   const navigationMenu = document.querySelector("[data-navigation-menu]");
   const navigationNavbar = navigationMenu.querySelector("[data-navigation-navbar]");
-  const navigationBgClouds = navigationMenu.querySelectorAll("[data-navigation-cloud]");
+  let navigationBgClouds = navigationMenu.querySelectorAll("[data-navigation-cloud]");
+  const navigationCloudsContainer = navigationMenu.querySelector("[data-navigation-clouds-container]");
   const openMenuButton = document.querySelector(".open-menu-btn");
   const closeMenuButton = navigationMenu.querySelector(".close-menu-btn");
+  const cloudTemplate = navigationMenu.querySelector("[data-navigation-cloud-template]");
 
+  // positioning clouds
+  const targetCoveragePercentage = 2;
+
+  const CLOUD_IMAGES = [
+    {
+      className: "navigation-menu__bg-cloud--center",
+      width: 400,
+      aspectRatio: 1.98,
+      imgURL: "./images/nav-cloud-center.webp"
+    },
+    {
+      className: "navigation-menu__bg-cloud--top-left",
+      width: 600,
+      aspectRatio: 2.32,
+      imgURL: "./images/nav-cloud-top-left.webp"
+    },
+    {
+      className: "navigation-menu__bg-cloud--top-right",
+      width: 800,
+      aspectRatio: 2.45,
+      imgURL: "./images/nav-cloud-top-right.webp"
+    },
+    {
+      className: "navigation-menu__bg-cloud--bottom-left",
+      width: 700,
+      aspectRatio: 2.1,
+      imgURL: "./images/nav-cloud-bottom-left.webp"
+    },
+    {
+      className: "navigation-menu__bg-cloud--bottom-right",
+      width: 740,
+      aspectRatio: 1.98,
+      imgURL: "./images/nav-cloud-bottom-right.webp"
+    },
+  ];
+
+  function randomizeClouds(targetCoveragePercentage) {
+    let currentCoverage = 0;
+    const skyArea = navigationCloudsContainer.clientWidth * navigationCloudsContainer.clientHeight;
+    const targetCoverage = skyArea * targetCoveragePercentage;
+
+    navigationCloudsContainer.innerHTML = '';
+
+    while (currentCoverage < targetCoverage) {
+      // console.log("randomizing clouds");
+      // console.log("targetCoverage: ", targetCoverage);
+      // console.log("currentCoverage: ", currentCoverage);
+      const cloudFragment = cloudTemplate.content.cloneNode(true);
+
+      const randomImageObj = CLOUD_IMAGES[Math.floor(Math.random() * CLOUD_IMAGES.length)];
+      
+      const cloudWrapper = cloudFragment.querySelector("[data-navigation-cloud]");
+      cloudWrapper.classList.add(randomImageObj.className);
+      cloudWrapper.style.width = `${randomImageObj.width}px`;
+      cloudWrapper.style.height = `${randomImageObj.width / randomImageObj.aspectRatio}px`;
+
+      const cloudImg = cloudFragment.querySelector("[data-navigation-cloud-image]");
+      cloudImg.src = randomImageObj.imgURL;
+      cloudImg.style.transform = `translateX(calc(-140% - ${navigationCloudsContainer.clientWidth}px))`;
+      
+      const cloudWidth = randomImageObj.width;
+      const cloudHeight = randomImageObj.width / randomImageObj.aspectRatio;
+
+      const xPos = (Math.random() * (navigationCloudsContainer.clientWidth + cloudWidth)) - (cloudWidth / 2);
+      const yPos = (Math.random() * (navigationCloudsContainer.clientHeight + cloudHeight)) - (cloudHeight / 2);
+
+      cloudWrapper.style.left = `${xPos}px`;
+      cloudWrapper.style.top = `${yPos}px`;
+
+      navigationCloudsContainer.appendChild(cloudFragment);
+
+      // Calculate coverage
+      const cloudRect = cloudWrapper.getBoundingClientRect();
+      const visibleWidth = Math.max(0, Math.min(cloudRect.right, navigationCloudsContainer.clientWidth) - Math.max(cloudRect.left, 0));
+      const visibleHeight = Math.max(0, Math.min(cloudRect.bottom, navigationCloudsContainer.clientHeight) - Math.max(cloudRect.top, 0));
+      const visibleArea = visibleWidth * visibleHeight;
+
+      currentCoverage += visibleArea;
+    }
+  }
+
+  // animating clouds
   const fadeInDuration = parseInt(getCSSPropertyValueFromRoot("--NAVBAR-fade-in-transition-duration"));
   const slideInBiggestDuration = parseInt(getCSSPropertyValueFromRoot("--NAVBAR-slide-in-biggest-transition-duration"));
 
@@ -21,6 +105,8 @@ if (document.querySelector("[data-navigation-menu]")) {
 
   function openMenuPanel() {
     navigationMenu.classList.add("show");
+    randomizeClouds(targetCoveragePercentage);
+    navigationBgClouds = navigationMenu.querySelectorAll("[data-navigation-cloud]");
 
     setTimeout(() => {
       navigationMenu.classList.add("fade-in");
