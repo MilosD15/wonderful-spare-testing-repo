f9e9645622c381836ba39c1eee03595c0c99bd58
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2023-04-12 07:49:33 +0200

    PRODUCT-HERO-redoing-scrollable-section

--- js/productHero.js
+++ js/productHero.js
@@ -9,8 +9,8 @@ if (document.querySelector("[data-product-hero-container]")) {
   window.addEventListener("load", adjustHeightOfProductHeroContainer);
   window.addEventListener("resize", adjustHeightOfProductHeroContainer);
   // make graphic part relative to scroll
-  window.addEventListener("load", makeGraphicPartRelativeToScroll);
-  window.addEventListener("scroll", makeGraphicPartRelativeToScroll);
+  // window.addEventListener("load", makeGraphicPartRelativeToScroll);
+  // window.addEventListener("scroll", makeGraphicPartRelativeToScroll);
   
 
   function adjustHeightOfProductHeroContainer() {
@@ -23,36 +23,36 @@ if (document.querySelector("[data-product-hero-container]")) {
     }
   }
 
-  function makeGraphicPartRelativeToScroll() {
-    removePositioningClasses(productHeroGraphicPart);
+  // function makeGraphicPartRelativeToScroll() {
+  //   removePositioningClasses(productHeroGraphicPart);
 
-    if (window.innerWidth < 1300) return;
-    if (productHeroGraphicPart.clientHeight >= productHeroContentPart.clientHeight) return;
+  //   if (window.innerWidth < 1300) return;
+  //   if (productHeroGraphicPart.clientHeight >= productHeroContentPart.clientHeight) return;
 
-    const currentScroll = document.documentElement.scrollTop;
-    const sectionScrollTop = getElementsScrollTop(productHeroContainer);
-    const scrollInsideSection = currentScroll - sectionScrollTop;
-    const windowHeight = window.innerHeight;
+  //   const currentScroll = document.documentElement.scrollTop;
+  //   const sectionScrollTop = getElementsScrollTop(productHeroContainer);
+  //   const scrollInsideSection = currentScroll - sectionScrollTop;
+  //   const windowHeight = window.innerHeight;
 
-    if (!isSectionInViewPort(productHeroContainer)) return;
-    if (scrollInsideSection <= windowHeight) {
-      productHeroGraphicPart.classList.add("absolute-top-0"); return;
-    }
+  //   if (!isSectionInViewPort(productHeroContainer)) return;
+  //   if (scrollInsideSection <= windowHeight) {
+  //     productHeroGraphicPart.classList.add("absolute-top-0"); return;
+  //   }
     
-    const difference = scrollInsideSection - windowHeight;
-    // const topExtentToBeCentered = (windowHeight - productHeroGraphicPart.clientHeight) / 2;
+  //   const difference = scrollInsideSection - windowHeight;
+  //   // const topExtentToBeCentered = (windowHeight - productHeroGraphicPart.clientHeight) / 2;
 
-    if (difference + productHeroGraphicPart.clientHeight > productHeroContainer.clientHeight) {
-      productHeroGraphicPart.classList.add("absolute-bottom-0"); return;
-    }
+  //   if (difference + productHeroGraphicPart.clientHeight > productHeroContainer.clientHeight) {
+  //     productHeroGraphicPart.classList.add("absolute-bottom-0"); return;
+  //   }
 
-    productHeroGraphicPart.classList.add("fixed-top-0");
-    // productHeroGraphicPart.style.top = `${difference}px`;
-  }
+  //   productHeroGraphicPart.classList.add("fixed-top-0");
+  //   // productHeroGraphicPart.style.top = `${difference}px`;
+  // }
 
-  function removePositioningClasses(productHeroGraphicPart) {
-    productHeroGraphicPart.classList.remove("absolute-top-0");
-    productHeroGraphicPart.classList.remove("absolute-bottom-0");
-    productHeroGraphicPart.classList.remove("fixed-top-0");
-  }
+  // function removePositioningClasses(productHeroGraphicPart) {
+  //   productHeroGraphicPart.classList.remove("absolute-top-0");
+  //   productHeroGraphicPart.classList.remove("absolute-bottom-0");
+  //   productHeroGraphicPart.classList.remove("fixed-top-0");
+  // }
 }
