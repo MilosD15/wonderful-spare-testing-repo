62a8bac786af563b21b8866ccf5eae39ffc3491a
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2023-04-12 16:42:40 +0200

    FAQ-fixing-issues-with-touch-and-mouse-events

--- js/faq.js
+++ js/faq.js
@@ -4,9 +4,48 @@ if (document.querySelector("[data-faq-section]")) {
   const faqSection = document.querySelector("[data-faq-section]");
   const faqLottiePlayer = faqSection.querySelector("[data-faq-lottie-player]");
 
-  document.body.addEventListener("click", e => {
+  document.body.addEventListener("mouseover", e => {
+    if (isTouchDevice()) return;
+
+    if (e.target.matches("[data-faq-question-btn]")) {
+      e.target.classList.add("on-hover");
+
+      e.target.addEventListener("mouseleave", e => {
+        if (isTouchDevice()) return;
+
+        if (e.target.matches("[data-faq-question-btn]")) {
+          e.target.classList.remove("on-hover");
+        }
+      }, { once: true });
+    }
+  });
+
+  document.body.addEventListener("mousedown", e => {
+    if (isTouchDevice()) return;
+
     if (e.target.matches("[data-faq-question-btn]")) {
       handleQuestionClick(e);
+    }
+  });
+
+  document.body.addEventListener("touchstart", e => {
+    if (e.target.matches("[data-faq-question-btn]")) {
+      handleQuestionClick(e);
+      const questionHoverText = e.target.dataset.questionHoverText;
+
+      if (questionHoverText === "Collapse answer") {
+        e.target.classList.add("on-hover");
+      }
+    }
+  });
+
+  document.body.addEventListener("touchend", e => {
+    if (e.target.matches("[data-faq-question-btn]")) {
+      const questionHoverText = e.target.dataset.questionHoverText;
+
+      if (questionHoverText === "Reveal answer") {
+        e.target.classList.remove("on-hover");
+      }
     }
   });
   
@@ -18,6 +57,10 @@ if (document.querySelector("[data-faq-section]")) {
     const questionHoverText = e.target.dataset.questionHoverText;
     const newQuestionHoverText = questionHoverText === "Reveal answer" ? "Collapse answer" : "Reveal answer";
     e.target.dataset.questionHoverText = newQuestionHoverText;
+  }
+
+  function isTouchDevice() {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
   }
 
   // lottie
