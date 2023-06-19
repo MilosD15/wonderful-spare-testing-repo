b98bb71cad0ea197f6a081d27c15bbefa7a62973
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2023-06-19 17:22:44 +0200

    FAQ-preview

---
+++ js/faq.js
@@ -0,0 +1,114 @@
+import { isSectionInViewPort } from "./additional-func.js";
+
+console.log("script loaded");
+
+if (document.querySelector("[data-faq-section]")) {
+  const faqSection = document.querySelector("[data-faq-section]");
+  const faqLottiePlayer = faqSection.querySelector("[data-faq-lottie-player]");
+
+  console.log("faq section...");
+
+  if (!isTouchDevice()) {
+    document.body.addEventListener("mouseenter", e => {
+      handleQuestionMouseOver(e);
+      console.log("mouseenter");
+    });
+    document.body.addEventListener("click", e => {
+      handleQuestionMouseDown(e);
+      console.log("click");
+    });
+  }
+
+  document.body.addEventListener("mouseover", e => {
+    handleQuestionMouseOver(e);
+    console.log("mouseover");
+  });
+  document.body.addEventListener("mousedown", e => {
+    handleQuestionMouseDown(e);
+    console.log("mousedown");
+  });
+
+  function handleQuestionMouseOver(e) {
+    if (isTouchDevice()) return;
+
+    if (e.target.matches("[data-faq-question-btn]")) {
+      e.target.classList.add("on-hover");
+
+      e.target.addEventListener("mouseleave", e => {
+        if (isTouchDevice()) return;
+
+        if (e.target.matches("[data-faq-question-btn]")) {
+          e.target.classList.remove("on-hover");
+        }
+      }, { once: true });
+    }
+  }
+
+  function handleQuestionMouseDown(e) {
+    if (isTouchDevice()) return;
+
+    if (e.target.matches("[data-faq-question-btn]")) {
+      handleQuestionClick(e);
+    }
+  }
+
+  document.body.addEventListener("touchstart", e => {
+    if (e.target.matches("[data-faq-question-btn]")) {
+      handleQuestionClick(e);
+      const questionHoverText = e.target.dataset.questionHoverText;
+
+      if (questionHoverText === "Collapse answer") {
+        e.target.classList.add("on-hover");
+      }
+    }
+  });
+
+  document.body.addEventListener("touchend", e => {
+    if (e.target.matches("[data-faq-question-btn]")) {
+      const questionHoverText = e.target.dataset.questionHoverText;
+
+      if (questionHoverText === "Reveal answer") {
+        e.target.classList.remove("on-hover");
+      }
+    }
+  });
+  
+  function handleQuestionClick(e) {
+    const questionContainer = e.target.closest("[data-faq-question]");
+    const answerContainer = questionContainer.querySelector("[data-faq-answer]");
+    $(answerContainer).slideToggle(500, "swing");
+  
+    const questionHoverText = e.target.dataset.questionHoverText;
+    const newQuestionHoverText = questionHoverText === "Reveal answer" ? "Collapse answer" : "Reveal answer";
+    e.target.dataset.questionHoverText = newQuestionHoverText;
+  }
+
+  function isTouchDevice() {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+  }
+
+  // lottie
+  let sectionWasAlreadyInViewport = false;
+
+  function reloadPlayer(player) {
+    player.load('../faq.lottie');
+  
+    player.addEventListener('ready', () => {
+      player.play();
+      player.setSpeed(0.9);
+    });
+  }
+
+  window.addEventListener("load", handleLottieAnimation);
+  window.addEventListener("scroll", handleLottieAnimation);
+
+  function handleLottieAnimation() {
+    if (isSectionInViewPort(faqSection) && sectionWasAlreadyInViewport === false) {
+      sectionWasAlreadyInViewport = true;
+      reloadPlayer(faqLottiePlayer);
+    }
+    if (!isSectionInViewPort(faqSection)) {
+      sectionWasAlreadyInViewport = false;
+    }
+  }
+}
