4e7ffcca58356f023b11ffc4010c0747ba9d2d44
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2023-07-01 17:18:39 +0200

    TY-SAMPLES-first-draft

---
+++ js/tySamples.js
@@ -0,0 +1,7 @@
+// imports
+import ElementParallax from "./ElementParallax.js";
+import { isSectionInViewPort, getCSSPropertyValueFromRoot } from "./additional-func.js";
+
+if (document.querySelector("[data-ty-samples-section]")) {
+  
+}
