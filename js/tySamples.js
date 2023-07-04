553d71e9151d320ae90fa09c9c45c1748015a790
Milos Dronjak <87735668+MilosD15@users.noreply.github.com>
2023-07-04 15:34:33 +0200

    TY-SAMPLES-airplane-animations-added

--- js/tySamples.js
+++ js/tySamples.js
@@ -3,5 +3,26 @@ import ElementParallax from "./ElementParallax.js";
 import { isSectionInViewPort, getCSSPropertyValueFromRoot } from "./additional-func.js";
 
 if (document.querySelector("[data-ty-samples-section]")) {
-  
+  // DOM elements
+  const tySamplesSection = document.querySelector("[data-ty-samples-section]");
+  const tySamplesAirplanePilotContainer = tySamplesSection.querySelector("[data-ty-samples-airplane-pilot-container]");
+
+  // variables
+  const tySamplesAirplaneTransitionDuration = parseInt(getCSSPropertyValueFromRoot("--TY-SAMPLES-SEC-airplane-transition-duration"));
+
+  // animate airplane and pilot
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+
+      tySamplesAirplanePilotContainer.classList.add("load-transition");
+
+      setTimeout(() => {
+        tySamplesAirplanePilotContainer.classList.add("airplane-flying");
+      }, tySamplesAirplaneTransitionDuration);
+
+      observer.unobserve(entry.target);
+    });
+  }, { threshold: 0.8 });
+  observer.observe(tySamplesSection);
 }
